c381d4611a4097516a31705648c8b58118801c90
Vincent Dibon <vincent.dibon@fjubots.io>
2022-12-04 11:17:31 +0100

    .

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ScanComponent } from './scan/scan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: AppComponent },
+  { path: '/menu', component: MenuComponent },
+  { path: '/scan', component: ScanComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/app/src/app/app.module.ts
+++ frontend/app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ScanComponent } from './scan/scan.component';
+import { MenuComponent } from './menu/menu.component';
+import { ViewerComponent } from './viewer/viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ScanComponent,
+    MenuComponent,
+    ViewerComponent
   ],
   imports: [
     BrowserModule,
